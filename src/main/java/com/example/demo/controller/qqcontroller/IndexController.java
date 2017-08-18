72097084bfb669c3f61d227b33a1169e761859fc
sl2020520 <1076396468@qq.com>
2017-08-18 14:04:49 +0800

    test jenkins

--- src/main/java/com/example/demo/controller/qqcontroller/IndexController.java
+++ src/main/java/com/example/demo/controller/qqcontroller/IndexController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class IndexController {
     @RequestMapping(value = "/toqqlogin",method = RequestMethod.GET)
     public String toQQLogin(){
+        System.out.println("0000000000000000000000000");
         return "redirect:https://graph.qq.com/";
     }
 
